bc3bf750f9299ace015666103bcaf2e738e7c1e6
Bryan Martínez <53927861+KinImoX@users.noreply.github.com>
2021-02-11 22:52:43 -0600

    Inicio del codigo
    Se agrega la plantilla del proyecto

---
+++ Gestion_Memoria/src/main/java/unah/so2/gestion_memoria/ParticionEstatica.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unah.so2.gestion_memoria;
+
+/**
+ *
+ * @author Bryan Martinez, Demsey Euceda, Freddy, Karen Bautista
+ */
+public class ParticionEstatica {
+    
+}
